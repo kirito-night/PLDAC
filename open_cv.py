ad5cbf2667b71e1954b331b908fde21e5ba2ce9c
yuanyuanzo <114346810+yuanyuanzo@users.noreply.github.com>
2023-02-19 00:55:08 +0100

    ts video saved

--- open_cv.py
+++ open_cv.py
@@ -7,10 +7,11 @@ import numpy as np
 '''
 该代码尝试使用背景差分法，完成了固定摄像头中，动态物体的提取。
 '''
-result = cv.VideoWriter('filename.avi', 
-                         cv.VideoWriter_fourcc(*'MJPG'),
-                         10, size)
+#定义输出文件
+Fourcc = cv.VideoWriter_fourcc(*'MPEG')
+out = cv.VideoWriter('result/nid/location_dectection_1.ts',Fourcc, 20, (1440,1080),False)
 tracker = EuclideanDistTracker() 
+mat=[]
 
 #有两种算法可选，KNN和MOG2，下面的代码使用KNN作为尝试
 algo='KNN'
@@ -18,14 +19,22 @@ if algo == 'MOG2':
     backSub = cv.createBackgroundSubtractorMOG2()
 else:
     backSub = cv.createBackgroundSubtractorKNN()
+
+
 #打开一个视频文件
 capture = cv.VideoCapture(cv.samples.findFileOrKeep('resource/video_juste_nid-test.ts'))
 #判断视频是否读取成功
 if not capture.isOpened():
     print('Unable to open')
     exit(0)
+
+
+
+
+
+
 #逐帧读取视频，进行相关分析
-while True:
+while (True):
     #读取视频的第一帧
     ret, frame = capture.read()
     if frame is None:
@@ -75,9 +84,26 @@ while True:
     cv.imshow('Frame', frame)
     #cv.imshow('FG Mask', fgMask)
     cv.imshow('Object',Object)
+
+    #将当前帧写入输出文件
+    frame.dtype=np.uint8
+    out.write(frame)
+    mat.append(boxer_ids)
+
     #每帧展示结束，等待30毫秒
     keyboard = cv.waitKey(30)
     #按q推出程序
-    if keyboard == 'q' or keyboard == 27:
+    if  keyboard == 27:
         break
+
+
+#存入npy文件
+mat=np.array(mat,dtype=object)
+np.save('result/nid/location_dectection.npy',mat)
+
+#释放资源
+capture.release()
+out.release()
+cv.destroyAllWindows()
+
 
