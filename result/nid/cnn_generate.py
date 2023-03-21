74005241f5f71aecb14553dcf2417cf719c957a1
yuanyuanzo <114346810+yuanyuanzo@users.noreply.github.com>
2023-03-21 16:02:50 +0100

    lpan

--- result/nid/cnn_generate.py
+++ result/nid/cnn_generate.py
@@ -1,9 +1,26 @@
-import matplotlib.pyplot as plt
+import cv2
 import numpy as np
-from scipy.signal import convolve2d
-import pandas as pd
-import cv2 as cv
-
-y1=np.load('result/nid/y_1300.npy')
-y2=np.load('result/nid/y_1200.npy')
-print(y1+y2)
+ 
+# 图片路径
+img = cv2.imread('extract/nid/001.jpg')
+a = []
+b = []
+ 
+ 
+def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        xy = "%d,%d" % (x, y)
+        a.append(x)
+        b.append(y)
+        cv2.circle(img, (x, y), 5, (0, 0, 255), thickness=-1)
+        cv2.putText(img, xy, (x, y), cv2.FONT_HERSHEY_PLAIN,1.0, (255, 255, 255), thickness=1)
+        cv2.imshow("image", img)
+        print(x,y)
+ 
+ 
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", on_EVENT_LBUTTONDOWN)
+cv2.imshow("image", img)
+cv2.waitKey(0)
+print(a, b)
+ 
